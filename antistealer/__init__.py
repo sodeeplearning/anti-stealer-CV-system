b164f78cb064891d5acaa87fa19ae874d5948820
Vitallya <vitaliy.petreev@gmail.com>
2025-02-21 17:20:21 +0300

    Added requests to chatgpt

--- antistealer/__init__.py
+++ antistealer/__init__.py
@@ -1,1 +1,8 @@
-from . import model
+from model import model_request
+from utils.videoprocessor import extract_frames
+
+
+def make_request(video_path: "str") -> str:
+    frame_bytes = extract_frames(video_path)
+    response = model_request(images=frame_bytes)
+    return response
--- antistealer/model.py
+++ antistealer/model.py
@@ -1,7 +1,20 @@
 import openai
 
 from credentials import api_key
+from config import system_prompt
+from config import model_name
 
 
 openai.api_key = api_key
 
+
+def model_request(images: list[bytes]) -> str:
+    user_content = [{"type": "image", "image": current} for current in images]
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "text": system_prompt},
+            {"role": "user", "content": user_content}
+        ]
+    )
+    return response["choices"][0]["message"]["content"]
